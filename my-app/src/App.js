6381488d7d90ed41c6a658539859588349d6144d
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-09-19 21:03:04 -0300

    feat:pagina Principal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function index(props) {
+return (
+<div className='row no-gutters'>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"/>
+    <div className='col-md-6 no-gutters'>
+        <div className='leftside d-flex justify-content-center align-items-center'>
+        <img src="../Images/image1.jpg" class="img-fluid" alt="Responsive " />
+            Curriculo
+        </div>
     </div>
-  );
+    <div className='col-md-6 no-gutters'>
+        <div className='rightside d-flex justify-content-center align-items-center'>
+            Projetos
+        </div>
+    </div>
+
+</div>
+);
 }
 
-export default App;
+export default index;
