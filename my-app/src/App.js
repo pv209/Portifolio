e6456629ac2c873dd1c24d8efb96c0302945e778
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-09-19 22:14:42 -0300

    feat:tela principal

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import "./App.css";
 
+
+
 function index(props) {
 return (
-<div className='row no-gutters'>
+<>
+<>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"/>
-    <div className='col-md-6 no-gutters'>
+</>
+<div className='row g-0'>
+
+    <div className='col-md-6 g-0'>
         <div className='leftside d-flex justify-content-center align-items-center'>
-        <img src="../Images/image1.jpg" class="img-fluid" alt="Responsive " />
+        
+        
             Curriculo
         </div>
     </div>
-    <div className='col-md-6 no-gutters'>
+    <div className='col-md-6 g-0'>
         <div className='rightside d-flex justify-content-center align-items-center'>
             Projetos
         </div>
     </div>
+    
 
 </div>
+</>
+ 
 );
 }
 
